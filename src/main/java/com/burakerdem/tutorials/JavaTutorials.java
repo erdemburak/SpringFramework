edb4720dd8f3da148d415e9ae7e48db17dbe5aab
erdemburak <burakerdem6@hotmail.com>
2022-09-01 20:42:31 +0300

    Hatırlatma

---
+++ src/main/java/com/burakerdem/tutorials/JavaTutorials.java
@@ -0,0 +1,65 @@
+package com.burakerdem.tutorials;
+
+import java.util.Arrays;
+
+public class JavaTutorials {
+    public static void main(String[] args) {
+        // Arrays.sort();
+        // this.
+        // super
+        // break continue return
+        // optional
+        // cast
+        // primitive type
+        // wrapper class
+        // Random
+        // Math
+        // %s %d %f --> printf()
+        // final Değişkenler(Sabitleyici) - Methodlar(Override Edilemez) - Class(Extend edilemez)
+        // static
+        // decoder encoder
+        // instanceof
+        // public private protected
+        // string
+        // StringBuilder
+        // StringTokenizer
+        // StringBuffer
+        // Enum
+        // Multithread
+
+        // S.O.L.I.D
+        // KISS
+        // DRY - Dont Repeat Yourself
+        // MONAD - Bir methodun çıktısı başka bir methodun girdisi anlamına gelir
+        // Collection List Set Map
+        // Liste: LinkedList - ArrayList
+        // Generic <>
+        // Iterator
+        // OOP
+        // Interface
+        // Acstract
+        // Inheritance
+        // Polymorphism
+
+        // UML - + __ (Unified model language)
+        // Class
+        // POJO
+        // Serileştirme
+        // IO
+        // JAR WAR
+        // Dağıtık programlama
+        // Server
+
+        // Constructor
+        // Exception
+        // Error
+
+        // Java 8 : Stream,map,filter,sort
+        // Optional
+        // Lambda Expression
+        // Method Referances ::
+
+        // Design Pattern
+    }
+
+}
